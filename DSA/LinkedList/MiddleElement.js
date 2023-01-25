8a05feab187537689a56776f5003f38ba84e5111
kal-el <74044780+PonganeshP@users.noreply.github.com>
2023-01-25 17:00:22 +0530

    Prblm STatement

--- DSA/LinkedList/MiddleElement.js
+++ DSA/LinkedList/MiddleElement.js
@@ -1,3 +1,38 @@
+/*
+Problem Description
+Given a linked list of integers, find and return the middle element of the linked list.
+
+NOTE: If there are N nodes in the linked list and N is even then return the (N/2 + 1)th element.
+
+
+Example Input
+Input 1:
+
+ 1 -> 2 -> 3 -> 4 -> 5
+Input 2:
+
+ 1 -> 5 -> 6 -> 2 -> 3 -> 4
+
+
+Example Output
+Output 1:
+
+ 3
+Output 2:
+
+ 2
+
+
+Example Explanation
+Explanation 1:
+
+ The middle element is 3.
+Explanation 2:
+
+ The middle element in even length linked list of length N is ((N/2) + 1)th element which is 2.
+
+*/
+
 class LinkedList {
   constructor(head = null) {
     this.head = head;
